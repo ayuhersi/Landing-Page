e1387a6eed5e3d29a69ceb7ff636ef6050e955f3
ayuhersi <ayub7hersi@gmail.com>
2021-11-28 23:52:40 +0300

    edit

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-$("#design").click(function () {
-  $("p").hide();
-});
-$("#design").click(function () {
-  $("p").show();
+$(document).ready(function () {
+  $("#img1").click(function () {
+    $("#ttt").show();
+    $("#img1").hide();
+  });
+  $("#ttt").mouseleave(function () {
+    $("#ttt").hide();
+    $("#img1").show();
+  });
+  $("#img2").click(function () {
+    $("#ttt2").show();
+    $("#img2").hide();
+  });
+  $("#ttt2").mouseleave(function () {
+    $("#ttt2").hide();
+    $("#img2").show();
+  });
+  $("#img3").click(function () {
+    $("#ttt3").show();
+    $("#img3").hide();
+  });
+  $("#ttt3").mouseleave(function () {
+    $("#ttt3").hide();
+    $("#img3").show();
+  });
 });
